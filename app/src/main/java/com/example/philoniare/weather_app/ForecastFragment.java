8e74a8d2eeb10bb059eb1916e2e1eb0e09ad44dd
philoniare <philoniare@gmail.com>
2016-02-12 18:41:53 -0500

    Added additional Intent for displaying weather detail

--- app/src/main/java/com/example/philoniare/weather_app/ForecastFragment.java
+++ app/src/main/java/com/example/philoniare/weather_app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.philoniare.weather_app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -62,6 +64,15 @@ public class ForecastFragment extends Fragment {
         mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent showDetailIntent = new Intent(getActivity(), DetailActivity.class)
+                    .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(showDetailIntent);
+            }
+        });
         return rootView;
     }
 
